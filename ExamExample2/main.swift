8bcc0f817329a86774939783a2dcad06508bd9ca
DmitryKotusev <dssa-1966@yandex.ru>
2018-06-16 15:38:37 +0300

    Initial Commit

---
+++ ExamExample2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamExample2
+//
+//  Created by Dmitry Kotusev on 6/16/18.
+//  Copyright © 2018 Dmitry Kotusev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
